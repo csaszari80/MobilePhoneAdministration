c47e884199d9ab7f03c1c593f561a15482dcc390
csaszariz <zoltan.csaszari@borsodchem.eu>
2018-02-22 10:58:35 +0100

    #3: Display és editor template kialakítása a ContractCategory-hoz

--- MobilePhoneAdministration/MobilePhoneAdministration/Migrations/Configuration.cs
+++ MobilePhoneAdministration/MobilePhoneAdministration/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-namespace MobilePhoneAdministration.Migrations
+Ôªønamespace MobilePhoneAdministration.Migrations
 {
     using Models;
     using System;
@@ -16,20 +16,29 @@ namespace MobilePhoneAdministration.Migrations
 
         protected override void Seed(MobilePhoneAdministration.Models.MobilePhoneAdministrationContext context)
         {
-            //KˆltsÈghelyek feltˆltÈse
+            //K√∂lts√©ghelyek felt√∂lt√©se
             context.CostPlaces.AddOrUpdate(x => x.CostCode,
-                new CostPlace(costcode: "511A", costname: "KˆltsÈghely 1", organizationcode: "11111", organizationname: "Szervezeti EgysÈg 1",forwardedbill: false),
-                new CostPlace(costcode: "444A", costname: "KˆltsÈghely 2", organizationcode: "11111", organizationname: "Szervezeti EgysÈg 2", forwardedbill: false)
+                new CostPlace(costcode: "511A", costname: "K√∂lts√©ghely 1", organizationcode: "11111", organizationname: "Szervezeti Egys√©g 1",forwardedbill: false),
+                new CostPlace(costcode: "444A", costname: "K√∂lts√©ghely 2", organizationcode: "11111", organizationname: "Szervezeti Egys√©g 2", forwardedbill: false)
             );
             context.SaveChanges();
 
-            //felhaszn·lÛk feltˆltÈse:
+            //felhaszn√°l√≥k felt√∂lt√©se:
             context.Users.AddOrUpdate(x => x.CPID,
-                new User(cpid: 111, name: "PÈlda PÈter", adlogin: "pelda1", active: true, editable: false, hidden: false, costplace: context.CostPlaces.Single(x => x.CostCode == "444A")),
-                new User(cpid: 112, name: "Mondjuk M·rton", adlogin: "mondjuk1", active: true, editable: true, hidden: false, costplace: context.CostPlaces.Single(x => x.CostCode == "444A")),
+                new User(cpid: 111, name: "P√©lda P√©ter", adlogin: "pelda1", active: true, editable: false, hidden: false, costplace: context.CostPlaces.Single(x => x.CostCode == "444A")),
+                new User(cpid: 112, name: "Mondjuk M√°rton", adlogin: "mondjuk1", active: true, editable: true, hidden: false, costplace: context.CostPlaces.Single(x => x.CostCode == "444A")),
                 new User(cpid: 113, name: "Legyen Lenke", adlogin: "legyen1", active: false, editable: false, hidden: false, costplace: context.CostPlaces.Single(x => x.CostCode == "511A"))
             );
             context.SaveChanges();
+
+            //szerz≈ëd√©st√≠pusok felt√∂lt√©se
+            context.ContractCategories.AddOrUpdate(x => x.Name,
+                new ContractCategory(name: "telefon/internet"),
+                new ContractCategory(name: "internet"),
+                new ContractCategory(name: "korl√°tozott adat")
+            );
+            context.SaveChanges();
+
         }
     }
 }
